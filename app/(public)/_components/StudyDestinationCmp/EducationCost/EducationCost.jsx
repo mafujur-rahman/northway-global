7fe8c741a5dba67dffccae00a310031ef5bd2135
mafujur-rahman <mdmafuj000@gmail.com>
2026-04-21 18:19:36 +0600

    fix the list and table responsive issue in study destination

--- app/(public)/_components/StudyDestinationCmp/EducationCost/EducationCost.jsx
+++ app/(public)/_components/StudyDestinationCmp/EducationCost/EducationCost.jsx
@@ -1,32 +1,37 @@
-import React from 'react'
-import TooltipBtn from '../../utlities/TooltipBtn/TooltipBtn'
+import React from 'react';
+import TooltipBtn from '../../utlities/TooltipBtn/TooltipBtn';
 
-export default function EducationCost ({educationCostData}) {
- 
+export default function EducationCost({ educationCostData }) {
   return (
-    <div className='section__spacing'>
-      {educationCostData?.map(item => (
-        <div key={item.id} className='grid lg:grid-cols-2 items-center gap-16 h-full'>
+    <div className="section__spacing">
+      {educationCostData?.map((item) => (
+        <div
+          key={item.id}
+          className="grid lg:grid-cols-2 items-center gap-16 h-full"
+        >
           <img
             src={item.image}
             alt={item.title}
-            className='w-full h-[50vh] object-cover rounded-lg '
+            className="w-full h-[50vh] object-cover rounded-lg"
           />
 
           <div>
             <TooltipBtn text={item.tooltipText} />
-            <h3 className='headingText'>
+            <h3 className="headingText">
               {item.title}{' '}
-              <span className='banner__color__title'>{item.colorText}</span>
+              <span className="banner__color__title">{item.colorText}</span>
             </h3>
 
-            <p className='heading__sub__text'>{item.description}</p>
+            <p className="heading__sub__text">{item.description}</p>
 
-            <div className='mt-10'>
-              <h4 className='text__medium mb-2'>{item.quickFactsTitle}</h4>
-              <ul className='list-disc list-inside heading__sub__text'>
-                {item.quickFacts.map(fact => (
-                  <li key={fact.id}>{fact.text}</li>
+            <div className="mt-10">
+              <h4 className="text__medium mb-2">{item.quickFactsTitle}</h4>
+              <ul className="heading__sub__text space-y-1">
+                {item.quickFacts.map((fact) => (
+                  <li key={fact.id} className="pl-5 relative">
+                    <span className="absolute left-0">•</span>
+                    {fact.text}
+                  </li>
                 ))}
               </ul>
             </div>
@@ -34,5 +39,5 @@ export default function EducationCost ({educationCostData}) {
         </div>
       ))}
     </div>
-  )
+  );
 }
